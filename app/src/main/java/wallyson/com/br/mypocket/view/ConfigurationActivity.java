6cd4342a9d8c2831a5fa5ba8e315ac02eb4d3d1d
WallysonNunes <wallyson.n.a.lima@gmail.com>
2016-07-18 17:30:48 -0300

    Create ConfigurationActivity

---
+++ app/src/main/java/wallyson/com/br/mypocket/view/ConfigurationActivity.java
@@ -0,0 +1,15 @@
+package wallyson.com.br.mypocket.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wallyson.com.br.mypocket.R;
+
+public class ConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
